e19d2387ac9119d13c4c3832433f899885b9373b
Andy Miller <amcoder@gmail.com>
2024-12-06 16:26:34 -0500

    Day 3

---
+++ day3/Day3Part1Solution.cs
@@ -0,0 +1,74 @@
+namespace Aoc2024;
+
+public class Day3Part1Solution : ISolution
+{
+    public int GetSolution()
+    {
+        var input = File.ReadAllText("day3/input");
+
+        var op1 = 0;
+        var op2 = 0;
+        var result = 0;
+
+        var state = "scan";
+
+        foreach (var c in input)
+        {
+            switch (state)
+            {
+                case "scan":
+                    state = c == 'm' ? "m" : "scan";
+                    break;
+                case "m":
+                    state = c == 'u' ? "u" : "scan";
+                    break;
+                case "u":
+                    state = c == 'l' ? "l" : "scan";
+                    break;
+                case "l":
+                    if (c == '(')
+                    {
+                        state = "op1";
+                        op1 = 0;
+                    }
+                    else
+                    {
+                        state = "scan";
+                    }
+                    break;
+                case "op1":
+                    if (char.IsDigit(c))
+                    {
+                        op1 = (op1 * 10) + c - '0';
+                    }
+                    else if (c == ',')
+                    {
+                        state = "op2";
+                        op2 = 0;
+                    }
+                    else
+                    {
+                        state = "scan";
+                    }
+                    break;
+                case "op2":
+                    if (char.IsDigit(c))
+                    {
+                        op2 = (op2 * 10) + c - '0';
+                    }
+                    else if (c == ')')
+                    {
+                        result += op1 * op2;
+                        state = "scan";
+                    }
+                    else
+                    {
+                        state = "scan";
+                    }
+                    break;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ day3/Day3Part2Solution.cs
@@ -0,0 +1,104 @@
+namespace Aoc2024;
+
+public class Day3Part2Solution : ISolution
+{
+    public int GetSolution()
+    {
+        var input = File.ReadAllText("day3/input");
+
+        var op1 = 0;
+        var op2 = 0;
+        var result = 0;
+        var enabled = true;
+
+        var state = "scan";
+
+        foreach (var c in input)
+        {
+            switch (state)
+            {
+                case "scan":
+                    state = enabled && c == 'm' ? "m" : c == 'd' ? "d" : "scan";
+                    break;
+                case "m":
+                    state = c == 'u' ? "u" : "scan";
+                    break;
+                case "u":
+                    state = c == 'l' ? "l" : "scan";
+                    break;
+                case "d":
+                    state = c == 'o' ? "o" : "scan";
+                    break;
+                case "o":
+                    state = c == 'n' ? "n" : c == '(' ? "do" : "scan";
+                    break;
+                case "n":
+                    state = c == '\'' ? "'" : "scan";
+                    break;
+                case "'":
+                    state = c == 't' ? "t" : "scan";
+                    break;
+                case "t":
+                    state = c == '(' ? "dont" : "scan";
+                    break;
+                case "do":
+                    if (c == ')')
+                    {
+                        enabled = true;
+                    }
+                    state = "scan";
+                    break;
+                case "dont":
+                    if (c == ')')
+                    {
+                        enabled = false;
+                    }
+                    state = "scan";
+                    break;
+                case "l":
+                    if (c == '(')
+                    {
+                        state = "op1";
+                        op1 = 0;
+                    }
+                    else
+                    {
+                        state = "scan";
+                    }
+                    break;
+                case "op1":
+                    if (char.IsDigit(c))
+                    {
+                        op1 = (op1 * 10) + c - '0';
+                    }
+                    else if (c == ',')
+                    {
+                        state = "op2";
+                        op2 = 0;
+                    }
+                    else
+                    {
+                        state = "scan";
+                    }
+                    break;
+                case "op2":
+                    if (char.IsDigit(c))
+                    {
+                        op2 = (op2 * 10) + c - '0';
+                    }
+                    else if (c == ')')
+                    {
+                        result += op1 * op2;
+                        state = "scan";
+                    }
+                    else
+                    {
+                        state = "scan";
+                    }
+                    break;
+            }
+        }
+
+        return result;
+    }
+}
